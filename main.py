f29904797e8341d755f8d7f11c8dfec8a713712b
Yumin Heo <rsdfq1230@g.skku.edu>
2022-12-27 06:13:15 +0900

    edit

--- main.py
+++ main.py
@@ -13,17 +13,13 @@ app = Flask(__name__)
 
 
 
-tok = AutoTokenizer.from_pretrained("./models/tok")
+tok = AutoTokenizer.from_pretrained("./models/kobart-myTokenizer")
 from transformers import BartConfig
-config=BartConfig.from_pretrained("./models/tok")
+config=BartConfig.from_pretrained("./models/kobart-myTokenizer")
 model = BartForConditionalGeneration(config=config)
 
-model.load_state_dict(torch.load('./models/bart_final_withoutStyle.bin',map_location=torch.device('cpu')))
+model.load_state_dict(torch.load('./models/kobart-myModel-positive.bin',map_location=torch.device('cpu')))
 model.eval()
-
-style_model = BartForConditionalGeneration(config=config)
-style_model.load_state_dict(torch.load('./models/enfp_final_1.bin',map_location=torch.device('cpu')))
-style_model.eval()
 
 
 @app.route('/quest_gen',methods=['GET','POST'])
@@ -32,11 +28,14 @@ def quest_gen():
         content=request.get_json(silent=True)
         place=content['keywords']
         k_place, k_space, k_object, k_action, k_location, k_time, k_place_modify, k_add =return_keys_random(place)
-        sent=f'[BOS] [MASK][ADNOM] {k_place}[PLACE] [MASK][ADVERB] [MASK][ADNOM] {k_object}[OBJECT] {k_action}[VERB] [/STYLE] [EOS]'
+        if k_object!='0':
+            sent=f'[BOS] [STYLE1] [MASK][ADNOM] {k_place}[PLACE] [MASK][ADVERB] [MASK][ADNOM] {k_object}[OBJECT] {k_action}[VERB] [/STYLE] [EOS]'
+        else:
+            sent=f'[BOS] [STYLE1] [MASK][ADNOM] {k_place}[PLACE] [MASK][ADVERB] {k_action}[VERB] [/STYLE] [EOS]'
         print(sent)
         
         input_ids=tok([sent],return_tensors='pt')['input_ids']
-        sent=tok.batch_decode(style_model.generate(input_ids,num_beams=5, do_sample=True, top_p=0.6, max_length=1024, num_return_sequences=1),skip_special_tokens=True)[0]
+        sent=tok.batch_decode(style_model.generate(input_ids, do_sample=True, top_p=0.6, max_length=1024, num_return_sequences=1),skip_special_tokens=True)[0]
         print(sent)
         data={
             'sentence':sent,
@@ -54,44 +53,34 @@ def home():
 def quest_gen_web():
     if request.method=='POST':
         content=request.get_json(silent=True)
-        sent=q_gen_final(content['k_place'],content['k_object'],content['k_action'],1)
+        sent=input_gen(content['k_place'],content['k_object'],content['k_action'],content['option'])
         input_ids=tok([sent],return_tensors='pt')['input_ids']
-        sent=tok.batch_decode(model.generate(input_ids,num_beams=2,no_repeat_ngram_size=3, do_sample=True,temperature=1.5, top_p=0.9, max_length=1024, num_return_sequences=1),skip_special_tokens=True)[0]
+        sent=tok.batch_decode(model.generate(input_ids,num_beams=2,no_repeat_ngram_size=3, do_sample=True,temperature=1.2, top_p=0.9, max_length=1024, num_return_sequences=1),skip_special_tokens=True)[0]
         return sent
 
-def q_gen_final(place,obj,action,choice):
-    sent=sent=f'[BOS] [MASK][ADNOM] {place}[PLACE] [MASK][ADVERB] [MASK][ADNOM] {obj}[OBJECT] {action}[VERB]'
-    if (choice==1):
-        sent+=' [EOS]'
+def input_gen(place, obj, action, opt):
+    sent=""
+    if opt==1:
+        if obj!='':
+            sent=f'[BOS] [ADNOM] {place}[PLACE] [ADVERB] [ADNOM] {obj}[OBJECT] {action}[VERB] [EOS]'
+        else:
+            sent=f'[BOS] [ADNOM] {place}[PLACE] [ADVERB] {action}[VERB] [EOS]'
+    elif opt==2:
+        if obj!='':
+            sent=f'[BOS] [STYLE1] [ADNOM] {place}[PLACE] [ADVERB] [ADNOM] {obj}[OBJECT] {action}[VERB] [/STYLE] [EOS]'
+        else:
+            sent=f'[BOS] [STYLE1] [ADNOM] {place}[PLACE] [ADVERB] {action}[VERB] [/STYLE] [EOS]'
+    elif opt==3:
+        if obj!='':
+            sent=f'[BOS] [STYLE1] [MASK][MASK][MASK] {place}[PLACE] [MASK][MASK][MASK] {obj}[OBJECT] {action}[VERB] [/STYLE] [EOS]'
+        else:
+            sent=f'[BOS] [STYLE1] [MASK][MASK][MASK] {place}[PLACE] [MASK][MASK][MASK] {action}[VERB] [/STYLE] [EOS]'
     else:
-        sent+=' [/STYLE] [EOS]'
+        if obj!='':
+            sent=f'[BOS] [STYLE2] [MASK][MASK][MASK] {place}[PLACE] [MASK][MASK][MASK] {obj}[OBJECT] {action}[VERB] [/STYLE] [EOS]'
+        else:
+            sent=f'[BOS] [STYLE2] [MASK][MASK][MASK] {place}[PLACE] [MASK][MASK][MASK] {action}[VERB] [/STYLE] [EOS]'
     return sent
-
-@app.route('/quest_gen_web_2',methods=['GET','POST'])
-def quest_gen_web_2():
-    if request.method=='POST':
-        content=request.get_json(silent=True)
-        sent=q_gen_final(content['k_place'],content['k_object'],content['k_action'],0)
-        input_ids=tok([sent],return_tensors='pt')['input_ids']
-        sent=tok.batch_decode(style_model.generate(input_ids,num_beams=2,temperature=1.1, no_repeat_ngram_size=3, do_sample=True, top_p=0.8, max_length=1024, num_return_sequences=1),skip_special_tokens=True)[0]
-        return sent
-
-@app.route('/style', methods=['GET','POST'])
-def change_style():
-    if request.method=='POST':
-        content=request.get_json(silent=True)
-        input_ids=tok(['[BOS] '+content['sent']+' [EOS]'],return_tensors='pt')['input_ids']
-        sent=tok.batch_decode(style_model.generate(input_ids,no_repeat_ngram_size=3, num_beams=2, do_sample=True, top_p=0.6, max_length=1024, num_return_sequences=1),skip_special_tokens=False)[0]
-        print(sent)
-        return sent[5:-5].replace('[BOS]','')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port = 5000)
-'''
-    test('교실')
-    test('갤러리')
-    test('쥐라기 파크')
-    test('복도')
-    test('옥상')
-    test('태양계')
-'''
--- static/script.js
+++ static/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     let genNum=0;
-    let savedString="";
-    $('#send').click(function(){
+    $('.send').click(function(e){
         let keyInfo={};
         let key_n=document.getElementsByClassName('k');
         let key_name=document.getElementsByClassName('kn');
@@ -17,6 +16,22 @@ $(document).ready(function(){
             let tmp='k_'+((key_name[i].innerText.split('/')[0]).toLowerCase());
             keyInfo[tmp]=key_n[i].value;
         }
+
+        let id=e.target.getAttribute('id')
+
+        if (id==='send1'){
+            keyInfo['option']=1;
+        }
+        else if (id==='send2'){
+            keyInfo['option']=2;
+        }
+        else if (id==='send3'){
+            keyInfo['option']=3;
+        }
+        else if (id==='send4'){
+            keyInfo['option']=4;
+        }
+
         info=keyInfo
         keyInfo=JSON.stringify(keyInfo);
         console.log(keyInfo);
@@ -27,111 +42,10 @@ $(document).ready(function(){
             contentType:'application/json',
             dataType: 'text',
 			success : function(data){                
-                let newRow2=document.getElementById('result').insertRow();
+                let newRow2=document.getElementById('result').insertRow(0);
                 newRow2.classList.add('sent');
                 newRow2.innerHTML=`<td class="s_" id="sent_${genNum}"></td>`;
                 console.log(data);
-
-                n=(info['k_action'].split(' ')).length; // action 길이 (이거만큼 뒤에)
-                dataList=data.split(' ');
-                dataLen=dataList.length;
-
-                if(dataList[dataLen-1]=='싶어요.' ){
-                    endi=dataLen; // 싶어요 </span>
-                    starti=dataLen-n-1; // <span> 게임을 하고
-                }
-                else{
-                    endi=dataLen;
-                    starti=dataLen-n;
-                }
-//                dataList.splice(endi,0,`%</span>`);
-//                dataList.splice(starti,0,`%<span class='blue'>`);
-
-                console.log(dataList[0])
-                sent='';
-                for (let d of dataList){
-                    console.log(d)
-                    if (d[0]=='%'){
-                        sent+=d.slice(1)
-                    }
-                    else
-                        sent+=(' '+d)
-                }
-
-                for (let k in info){
-                    let regex = new RegExp(`${info[k]}`);
-                    if (info[k]!=''){
-                        sent=sent.replace(regex,`<span class='blue'>${info[k]}</span>`);
-                    }
-                    console.log(sent);
-                }
-
-                document.getElementById(`sent_${genNum}`).innerHTML=sent;
-                genNum+=1;
-                savedString=data;
-			},
-            error : function(){
-				console.log("getSentence_fail");
-			}
-		});
-    })
-    $('#send2').click(function(){
-        let keyInfo={};
-        let key_n=document.getElementsByClassName('k');
-        let key_name=document.getElementsByClassName('kn');
-
-        keyInfo={
-            'k_place_modify': '0',
-            'k_location': '0',
-            'k_time': '0',
-            'k_add': '0',
-        }
-
-        for (let i=0; i<key_n.length ; i++) {
-            let tmp='k_'+((key_name[i].innerText.split('/')[0]).toLowerCase());
-            keyInfo[tmp]=key_n[i].value;
-        }
-        info=keyInfo
-        keyInfo=JSON.stringify(keyInfo)
-        console.log(keyInfo)
-        $.ajax({
-			type:'post',   //post 방식으로 전송
-			url: '/quest_gen_web_2',
-			data: keyInfo,
-            contentType:'application/json',
-            dataType: 'text',
-			success : function(data){
-                let newRow2=document.getElementById('result').insertRow();
-                newRow2.classList.add('sent');
-                newRow2.innerHTML=`<td class="s_" id="sent_${genNum}"></td>`;
-                console.log(data);
-
-                n=(info['k_action'].split(' ')).length; // action 길이 (이거만큼 뒤에)
-                dataList=data.split(' ');
-                dataLen=dataList.length;
-
-                console.log(dataList[0])
-                sent='';
-                for (let d of dataList){
-                    console.log(d)
-                    if (d[0]=='%'){
-                        sent+=d.slice(1)
-                    }
-                    else
-                        sent+=(' '+d)
-                }
-
-                for (let k in info){
-                    let regex = new RegExp(`${info[k]}`);
-                    if (info[k]!=''){
-                        sent=sent.replace(regex,`<span class='blue'>${info[k]}</span>`);
-                    }
-                    console.log(sent);
-                }
-
-                document.getElementById(`sent_${genNum}`).innerHTML=sent;
-                genNum+=1;
-                savedString=data;
 			},
             error : function(){
 				console.log("getSentence_fail");
